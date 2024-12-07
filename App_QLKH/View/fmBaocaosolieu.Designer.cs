86dd029a789fe1e33363848659a75058e0ce48d0
Lò Văn Anh <anhlo2819@gmail.com>
2024-12-07 14:37:28 +0700

    update packages cho báo cáo số liệu

--- App_QLKH/View/fmBaocaosolieu.Designer.cs
+++ App_QLKH/View/fmBaocaosolieu.Designer.cs
@@ -28,19 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(370, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Báo cáo số liệu";
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(87, 193);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(825, 401);
+            this.reportViewer1.TabIndex = 1;
             // 
             // fmBaocaosolieu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1028, 666);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.label1);
             this.Name = "fmBaocaosolieu";
             this.Text = "fmBaocaosolieu";
+            this.Load += new System.EventHandler(this.fmBaocaosolieu_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
     }
 }
--- App_QLKH/View/fmBaocaosolieu.cs
+++ App_QLKH/View/fmBaocaosolieu.cs
@@ -16,5 +16,11 @@ namespace App_QLKH.View
         {
             InitializeComponent();
         }
+
+        private void fmBaocaosolieu_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
     }
 }
